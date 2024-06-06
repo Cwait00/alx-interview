64780760fc36fd9c84c219d47d0758d87ef9cc8f
cwait00 <cwaitanobongoza@gmail.com>
2024-06-06 04:03:49 -0700

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,52 +2,44 @@
 
 const request = require('request');
 
-const movieID = process.argv[2];
-const baseURL = 'https://swapi.dev/api/films/';
-
-function fetchCharacters(movieURL) {
-  return new Promise((resolve, reject) => {
-    request(movieURL, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`Failed to fetch data: ${response.statusCode}`));
-      } else {
-        const filmData = JSON.parse(body);
-        const charactersURLs = filmData.characters;
-        resolve(charactersURLs);
-      }
-    });
-  });
+// Get movie ID from command line arguments
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
-async function printCharacters(movieID) {
-  const movieURL = `${baseURL}${movieID}/`;
+// API endpoint for the specified movie
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-  try {
-    const charactersURLs = await fetchCharacters(movieURL);
-    for (const characterURL of charactersURLs) {
-      const characterData = await fetchCharacter(characterURL);
-      console.log(characterData.name);
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-function fetchCharacter(characterURL) {
+const fetchCharacter = (url) => {
   return new Promise((resolve, reject) => {
-    request(characterURL, (error, response, body) => {
+    request(url, (error, response, body) => {
       if (error) {
         reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`Failed to fetch data: ${response.statusCode}`));
       } else {
         const characterData = JSON.parse(body);
-        resolve(characterData);
+        resolve(characterData.name);
       }
     });
   });
-}
+};
 
-printCharacters(movieID);
+request(apiUrl, async (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters;
+
+  for (const character of characters) {
+    try {
+      const name = await fetchCharacter(character);
+      console.log(name);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+});
