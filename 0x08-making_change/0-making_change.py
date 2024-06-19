db46b73e8c0bddffcaf881522c0fbd74e1e60812
cwait00 <cwaitanobongoza@gmail.com>
2024-06-19 07:50:38 -0700

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+This module contains the makeChange function.
+"""
+
 
 def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given total amount.
+    :param coins: List of coin denominations.
+    :param total: The total amount.
+    :return: Fewest number of coins needed to meet the total,
+    or -1 if it cannot be met.
+    """
     if total <= 0:
         return 0
 
@@ -16,6 +27,7 @@ def makeChange(coins, total):
     return dp[total] if dp[total] != float('inf') else -1
 
 
+# Main function for testing
 if __name__ == "__main__":
     print(makeChange([1, 2, 25], 37))  # Expected output: 7
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))  # Expected output: -1
