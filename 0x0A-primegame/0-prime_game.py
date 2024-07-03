e6907cc97bc2e9ce188b32785b5d4b96527ac780
cwait00 <cwaitanobongoza@gmail.com>
2024-07-03 05:05:17 -0700

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -62,9 +62,14 @@ def sieve(n):
         set: Set of all prime numbers less than or equal to n.
     """
     primes = set(range(2, n + 1))
-    for i in range(2, int(n ** 0.5) + 1):
-        if i in primes:
-            primes.difference_update(set(range(i * i, n + 1, i)))
+    is_prime = [True] * (n + 1)
+    for p in range(2, int(n ** 0.5) + 1):
+        if is_prime[p]:
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+    for p in range(2, n + 1):
+        if is_prime[p]:
+            primes.add(p)
     return primes
 
 
