73945185b3e7f336bb617320b4dcf6fe7c4ad5d0
cwait00 <cwaitanobongoza@gmail.com>
2024-07-09 09:57:47 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,36 +10,56 @@ def canUnlockAll(boxes):
     Returns:
     bool: True if all boxes can be opened, False otherwise.
     """
-
     if not boxes:
         return False
 
-    # Number of boxes
     n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
 
-    # Set to keep track of visited boxes
-    visited = set()
-    visited.add(0)
+    while stack:
+        current = stack.pop()
+        for key in boxes[current]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
 
-    queue = [0]
-
-    while queue:
-        current_box = queue.pop(0)
-
-        for key in boxes[current_box]:
-            if key not in visited and key < n:
-                visited.add(key)
-                queue.append(key)
-
-    return len(visited) == n
+    return all(visited)
 
 
 if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
     print(canUnlockAll(boxes))  # Expected output: True
 
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    boxes = [
+        [1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]
+    ]
     print(canUnlockAll(boxes))  # Expected output: True
 
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    boxes = [
+        [1, 2], [0], [3], [4], [5], [6], [7], [8], [9], [10], [11], []
+    ]
+    print(canUnlockAll(boxes))  # Expected output: False
+
+    boxes = [
+        [4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]
+    ]
+    print(canUnlockAll(boxes))  # Expected output: True
+
+    boxes = [[0]]
+    print(canUnlockAll(boxes))  # Expected output: True
+
+    boxes = [
+        [10, 3, 8, 9, 6, 5, 8, 1], [8, 5, 3, 7, 1, 8, 6], [5, 1, 9, 1], [],
+        [6, 6, 9, 4, 3, 2, 3, 8, 5], [9, 4], [4, 2, 5, 1, 1, 6, 4, 5, 6],
+        [9, 5, 8, 8], [6, 2, 8, 6]
+    ]
+    print(canUnlockAll(boxes))  # Expected output: True
+
+    boxes = [
+        [7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3],
+        [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7],
+        [4, 2, 9, 6, 6, 5, 5]
+    ]
     print(canUnlockAll(boxes))  # Expected output: False
